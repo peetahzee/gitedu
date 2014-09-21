34af86f4106ff293e6bd0574994338c3ced7d6ba
Peter Zhang <p@peetahzee.com>
2014-09-21 00:47:19 -0400

    DB

--- app/db.py
+++ app/db.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import constants
 
 def get_db():
 	# an Engine, which the Session will use for connection
 	# resources
-	engine = create_engine('postgresql://peter:tiger@localhost/gitedu')
+	engine = create_engine(constants.DB_URL)
 
 	# create a configured "Session" class
 	session = sessionmaker(bind=engine)
